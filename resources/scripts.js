c787b9255055c10a0d6ac6d5aa0b53eec68e27d5
Chris <christopher.hampson58@gmail.com>
2023-04-25 13:10:59 +0100

    Finish site, tidy old stuff

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,3 +1,27 @@
-window.onload = function() {
-    document.getElementsByTagName("body")[0].classList.remove("preload");
-}
+window.onload = function () {
+  document.getElementsByTagName("body")[0].classList.remove("preload");
+};
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    document
+      .getElementById("sticky-header")
+      .classList.add("background-light-blue");
+    document
+      .getElementById("sticky-header")
+      .classList.remove("colour-light-blue");
+    document
+      .getElementById("sticky-header")
+      .classList.add("colour-chocolate");
+  } else {
+    document
+      .getElementById("sticky-header")
+      .classList.remove("background-light-blue");
+    document
+      .getElementById("sticky-header")
+      .classList.add("colour-light-blue");
+    document
+      .getElementById("sticky-header")
+      .classList.remove("colour-chocolate");
+  }
+};
